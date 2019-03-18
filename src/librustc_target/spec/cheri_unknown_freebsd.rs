bf2d140d1a5373f3e622de008b6b9a3977756fd2
Nicholas Sim <nicholassimws@gmail.com>
2019-03-18 18:02:11 +0000

    librustc_target: update cheri spec

--- src/librustc_target/spec/cheri_unknown_freebsd.rs
+++ src/librustc_target/spec/cheri_unknown_freebsd.rs
@@ -8,28 +8,28 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use spec::{LinkerFlavor, Target, TargetOptions, TargetResult};
+use spec::{LinkerFlavor, Target, TargetResult};
 
 pub fn target() -> TargetResult {
+    let mut base = super::freebsd_base::opts();
+    base.cpu = "cheri128".to_string();
+    base.features = "+mips64r2".to_string();
+    base.max_atomic_width = Some(64);
+    base.has_elf_tls = false;
+    base.pre_link_args.get_mut(&LinkerFlavor::Gcc).unwrap().push("-mabi=purecap".to_string());
+    base.pre_link_args.get_mut(&LinkerFlavor::Gcc).unwrap().push("-fPIC".to_string());
+
     Ok(Target {
         llvm_target: "cheri-unknown-freebsd".to_string(),
         target_endian: "big".to_string(),
-        target_pointer_width: "64".to_string(),
+        target_pointer_width: "128".to_string(),
         target_c_int_width: "32".to_string(),
-        data_layout: "E-m:e-pf200:128:128:128:64-i8:8:32-i16:16:32-i64:64-n32:64-S128".to_string(),
+        data_layout: "E-m:e-pf200:128:128:128:64-i8:8:32-i16:16:32-i64:64-n32:64-S128-A200-P200-G200".to_string(),
         arch: "cheri".to_string(),
         target_os: "freebsd".to_string(),
         target_env: String::new(),
         target_vendor: "unknown".to_string(),
         linker_flavor: LinkerFlavor::Gcc,
-        options: TargetOptions {
-            // NOTE(mips64r2) matches C toolchain
-            cpu: "mips64r2".to_string(),
-            features: "+mips64r2".to_string(),
-            max_atomic_width: Some(64),
-            has_elf_tls: false,
-
-            ..super::freebsd_base::opts()
-        },
+        options: base,
     })
 }
